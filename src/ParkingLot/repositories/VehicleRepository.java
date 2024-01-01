9776f8405079a240624575dde99ab83c80409afc
Swatantra Prakash <swatantra.prakash@karexpert.com>
2024-01-01 21:55:27 +0530

    Code Added For Parking Lot

--- src/ParkingLot/repositories/VehicleRepository.java
+++ src/ParkingLot/repositories/VehicleRepository.java
@@ -3,7 +3,7 @@ package ParkingLot.repositories;
 import ParkingLot.models.Vehicle;
 
 import java.util.Optional;
-
+//Need to use JPA
 public class VehicleRepository {
     public Optional<Vehicle> getVehicleByNumber(String number){
         return Optional.empty();
